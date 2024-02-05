eaffd872e7aa4ed3158a788612a71e95e28eff08
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 11:27:21 +0200

    menu insertion

--- my-website/src/Components/navbar/navbar.jsx
+++ my-website/src/Components/navbar/navbar.jsx
@@ -4,7 +4,25 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <>
-      <nav className='nav-wrapper'></nav>
+      <nav className='nav-wrapper'>
+        <div className='nav-content'>
+          <img className='mylogo' src="" alt="logo" />
+          <ul>
+            <li>
+              <a className='menu-item' href=''>About</a>
+            </li>
+            <li>
+              <a className='menu-item' href=''>Services</a>
+            </li>
+            <li>
+              <a className='menu-item' href=''>Work</a>
+            </li>
+            <li>
+              <a className='menu-item' href=''>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
