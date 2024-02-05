79d08caf30bdce301031745a24fd5ec421db22d3
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 17:12:49 +0200

    home message

--- my-website/src/Components/Home/Home.jsx
+++ my-website/src/Components/Home/Home.jsx
@@ -3,7 +3,13 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="home-container">
+            <div className="home-content">
+                <h2> Welcome, Embark on a journey through landscape of innovation.</h2>
+                <hr />
+                <p>Whether you are seeking a developer or to bring ideas to life or seeking innovative solutions, you are on the right place.</p>
+            </div>
+        </section>
     )
 }
 
