91f5f616b7576833fd1c6eb004db8be0ea5b86e1
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 10:21:55 +0200

    pictures insertion

---
+++ my-website/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import navbar from './Components/NavBar/navbar';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      < navbar />
+    </>
+  );
+}
+
+export default App;
