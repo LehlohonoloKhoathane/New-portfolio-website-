83548d2c71a5ee21ceab0218b476bbee490267c6
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 13:29:23 +0200

    mobileupdate

---
+++ my-website/src/Components/navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './MobileNav.css'
+
+const MobileNav = ( {isOpen, toggleMenu}) => {
+    return (
+        <div>MobileNav</div>
+    )
+}
+
+export default MobileNav;
