f6872fc026d61a5e89ae4ae5fb00d2c0a8e66be9
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 17:33:26 +0200

    horizontal line on home

--- my-website/src/Components/Home/Home.jsx
+++ my-website/src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <section className="home-container">
             <div className="home-content">
                 <h2> Welcome, Embark on a journey through landscape of innovation.</h2>
-                <hr />
+                <hr/>
                 <p>Whether you are seeking a developer or to bring ideas to life or seeking innovative solutions, you are on the right place.</p>
             </div>
         </section>
