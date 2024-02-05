e0f0ec7a078f6ff257c28f6d15820e7b51ad5608
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 18:28:42 +0200

    cchanges of buttons

--- my-website/src/Components/Home/Home.jsx
+++ my-website/src/Components/Home/Home.jsx
@@ -6,8 +6,11 @@ const Home = () => {
         <section className="home-container">
             <div className="home-content">
                 <h2> Welcome, Embark on a journey through landscape of innovation.</h2>
-                <hr/>
+                <hr className="custom-hr"/>
                 <p>Whether you are seeking a developer or to bring ideas to life or seeking innovative solutions, you are on the right place.</p>
+
+                <a href=""><span>See Work</span></a>
+                <a href=""><span>Downoload Cv</span></a>
             </div>
         </section>
     )
