5237666eb3296aa5ddd76f4234e169cbb2b7091a
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-05 16:31:55 +0200

    creating home components

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './App.css'
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <div className='container'>
+        <Home />
+      </div>
     </>
   );
 };
