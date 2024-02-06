96bcf25a517476f6172015fa908d7e86c5b61121
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-06 17:14:41 +0200

    addition of other services

--- my-website/src/Components/Services/Services.jsx
+++ my-website/src/Components/Services/Services.jsx
@@ -14,9 +14,19 @@ const Services = () =>{
                     <p>I build dynamic and interactive web applications tailored to your specific needs.</p>
                 </div>
                 <div className="Service-item">
-                    <img src="./assets/images/web_dev.png" alt="" />
-                    <h5>Web Developer</h5>
-                    <p>I build dynamic and interactive web applications tailored to your specific needs.</p>
+                    <img src="./assets/images/mobile_dev.png" alt="" />
+                    <h5>Mobile App Developer</h5>
+                    <p>I craft visually appealing, user-friendly mobile apps for seamless experiences.</p>
+                </div>
+                <div className="Service-item">
+                    <img src="./assets/images/mobile_dev.png" alt="" />
+                    <h5>Data Analysis</h5>
+                    <p>I examine and interpret data to extract meaningful  insights and make informed decisions.</p>
+                </div>
+                <div className="Service-item">
+                    <img src="./assets/images/mobile_dev.png" alt="" />
+                    <h5>Mobile App Developer</h5>
+                    <p>I craft visually appealing, user-friendly mobile apps for seamless experiences.</p>
                 </div>
             </div>
         </section>
