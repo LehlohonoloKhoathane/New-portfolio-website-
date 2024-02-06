e34034c0448fdf121b7030356c8456cd658a97db
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-06 16:44:30 +0200

    Services css

--- my-website/src/Components/Services/Services.jsx
+++ my-website/src/Components/Services/Services.jsx
@@ -11,7 +11,7 @@ const Services = () =>{
                 <div className="Service-item">
                     <img src="./assets/images/web_dev.png" alt="" />
                     <h5>Web Developer</h5>
-                    <p></p>
+                    <p>I build dynamic and interactive web applications tailored to your specific needs.</p>
                 </div>
             </div>
         </section>
