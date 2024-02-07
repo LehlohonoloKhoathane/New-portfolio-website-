debcb7a85c771167351b879f4fa013be3d393ae2
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-07 10:33:22 +0200

    adding contact section

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Services from './Components/Services/Services';
 import Work from './Components/Work/Work';
+import Contact from './Components/Contact/Contact';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <About/>
         <Services/>
         <Work />
+        <Contact/>
       </div>
     </>
   );
---
+++ my-website/src/Components/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Contact.css'
+
+const Contact = () =>{
+    return (
+        <section className="contact-container">
+            <h2>Connect with Me</h2>
+            <div className="contact-content">
+                
+            </div>
+        </section>
+    )
+}
+
+export default Contact
