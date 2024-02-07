d1e014e1d9f5bc806f3085d1ac1b68d19251c578
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-07 14:20:36 +0200

    footer

--- my-website/src/Components/Footer/Footer.jsx
+++ my-website/src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <h3>Socials</h3>
                 <div className="socials"></div>
             </div>
-
+            <div className="footer"> Copyright &copy; 2024. All rights reserved | Designed & Developed by Lehlohonolo Khoathane</div>
         </div>
        </section>
     )
