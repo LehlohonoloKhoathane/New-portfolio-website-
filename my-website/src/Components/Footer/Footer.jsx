58e67917baf0a8b333a63426c0bdc67bccf40584
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-07 15:30:09 +0200

    adding socials

--- my-website/src/Components/Footer/Footer.jsx
+++ my-website/src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import './Footer.css';
+import { FaGithub } from "react-icons/fa6";
+import { BsLinkedin } from "react-icons/bs";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -7,6 +10,9 @@ const Footer = () => {
         <div className="footer-content">
             <div className="footer-social">
                 <h3>Socials</h3>
+                <FaGithub />
+                <BsLinkedin />
+                <FaInstagram />
                 <div className="socials"></div>
             </div>
             <div className="footer"> Copyright &copy; 2024. All rights reserved | Designed & Developed by Lehlohonolo Khoathane</div>
