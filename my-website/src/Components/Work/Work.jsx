c235287dbf9b913df772c23935916be0cf6706fb
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-08 14:20:43 +0200

    projects slide show

--- my-website/src/Components/Work/Work.jsx
+++ my-website/src/Components/Work/Work.jsx
@@ -19,18 +19,16 @@ const Work = () => {
                 <p>Witness the harmony of <span>design</span> and <span>functionality</span>, and immerse yourself in the visual allure of <span>innovations</span>. Let my creations speak for themselves.</p>
                 <div className="work-items">
                     <Slider {...settings}>
-                        <div className="work-item-in">
-                            <img src="./assets/images/Dashboard.png" alt="project1" />
-                            <p>This is one of the data analysis projects that was created using power BI and SQL for a student dashboard.</p>
+                    {data.map((d) => (
+                        <div className="service-item">
+                            <div className="service-image">
+                                <img src={d.img} alt="" />
+                            </div>
+                            <div>
+                                <p>{d.description}</p>
+                            </div>
                         </div>
-                        <div className="work-item-in">
-                            <img src="./assets/images/websitecol.png" alt="project2" />
-                            <p>This website is for an IT group of young people to make them available on the internet and do some work.</p>
-                        </div>
-                        <div className="work-item-in">
-                            <img src="./assets/images/festival.png" alt="project3" />
-                            <p>Crafted this immersive Cape Town Festival website experience, seamlessly blending sleek design with responsive functionality. It is not hosted as yet.</p>
-                        </div>
+                    ))}
                     </Slider>
                 </div>
                 <div className="work-testimonials">
@@ -66,7 +64,26 @@ const Work = () => {
     );
 };
 
+
+
+const data = [
+    {
+        img: `./assets/images/festival.png`,
+        description: `Crafted this immersive Cape Town Festival website experience, seamlessly blending sleek design with responsive functionality. It is not hosted as yet.`
+    },
+    {
+        img: `./assets/images/Dashboard.png`,
+        description: `This is one of the data analysis projects that was created using power BI and SQL for a student dashboard.`
+    },
+    {
+        img: `./assets/images/websitecol.png`,
+        description: `This website is for an IT group of young people to make them available on the internet and do some work.`
+    }
+]
+
+
 export default Work;
+
 
 
 // const Work = () => {
