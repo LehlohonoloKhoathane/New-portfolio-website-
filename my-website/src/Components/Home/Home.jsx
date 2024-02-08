b3af48eaee23cf43d1b15ab3e8a13bac11d8bd66
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-08 15:43:24 +0200

    alignments on navbar

--- my-website/src/Components/Home/Home.jsx
+++ my-website/src/Components/Home/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
                 <h2> Welcome, Embark on a journey through landscape of innovation.</h2>
                 <hr className="custom-hr"/>
                 <p>Whether you are seeking a developer or to bring ideas to life or seeking innovative solutions, you are on the right place.</p>
-
-                <a href=""><span>See Work</span></a>
-                <a href=""><span>Downoload Cv</span></a>
+                <div className="links-container">
+                    <a href=""><span>See Work</span></a>
+                    <a href=""><span>Downoload Cv</span></a>
+                </div>
             </div>
         </section>
     )
