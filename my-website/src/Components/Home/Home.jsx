4f8478254116051b49d995fb683c9f212b1d78cd
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-12 09:01:13 +0200

    home mobile responsiveness

--- my-website/src/Components/Home/Home.jsx
+++ my-website/src/Components/Home/Home.jsx
@@ -5,9 +5,13 @@ const Home = () => {
     return (
         <section className="home-container">
             <div className="home-content">
-                <h2> Welcome, Embark on a journey through landscape of innovation.</h2>
+                <div className="heading-container">
+                    <h2> Welcome, Embark on a journey through landscape of innovation.</h2>
+                </div>
                 <hr className="custom-hr"/>
-                <p>Whether you are seeking a developer or to bring ideas to life or seeking innovative solutions, you are on the right place.</p>
+                <div className="paragraph-container">
+                    <p>Whether you are seeking a developer or to bring ideas to life or seeking innovative solutions, you are on the right place.</p>
+                </div>
                 <div className="links-container">
                     <a href=""><span>See Work</span></a>
                     <a href=""><span>Downoload Cv</span></a>
