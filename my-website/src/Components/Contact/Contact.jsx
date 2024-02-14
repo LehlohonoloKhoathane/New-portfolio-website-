9074c2339dfda3382db923470f0422850e0d79fe
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-14 13:20:46 +0200

    commits on firebase

--- my-website/src/Components/Contact/Contact.jsx
+++ my-website/src/Components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import './FirebaseConfig';
+// import './FirebaseConfig';
 import {getFirestore, addDoc, collection} from 'firebase/firestore'
 import './Contact.css';
 import { BiMap } from "react-icons/bi";
