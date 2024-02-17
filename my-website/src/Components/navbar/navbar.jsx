ee19bdef489cf40c041f2367e8f1b5658ee692c1
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-17 14:02:31 +0000

    addition of comments on navbar

--- my-website/src/Components/navbar/navbar.jsx
+++ my-website/src/Components/navbar/navbar.jsx
@@ -4,14 +4,18 @@ import MobileNav from './MobileNav/MobileNav';
 // import { FaBars, FaTimes } from 'react-icons/fa'
 
 const NavBar = () => {
+   //Managing the mobile menu
   const [openMenu, setOpenMenu] = useState(false);
+  //Managing the visibility of the navbar
   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
   const [visible, setVisible] = useState(true);
 
+   //Function to toggle the mobile menu
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
   };
-
+  
+  //Function to handle scroll event and adjust visibility of navbar
   const handleScroll = () => {
     const currentScrollPos = window.pageYOffset;
     setVisible(
@@ -21,18 +25,24 @@ const NavBar = () => {
     setPrevScrollPos(currentScrollPos);
   };
 
+  //Effect to add scroll event listener when component mounts
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
+     //Cleanup function to remove event listener when component unmounts
     return () => window.removeEventListener('scroll', handleScroll);
   });
 
   return (
     <>
+    {/* Render the mobile navigation menu */}
       <MobileNav isOpen={openMenu} toggleMenu={toggleMenu}/>
 
+      {/* Render the desktop navigation */}
       <nav  className={`nav-wrapper ${visible ? 'visible' : 'hidden'}`} >
         <div className='nav-content'>
+           {/* Logo */}
           <img className='mylogo' src="./assets/images/myLogo.png" alt="logo" />
+           {/* Menu items */}
           <ul>
             <li>
               <a className='menu-item' href='#about-container'>About</a>
@@ -48,6 +58,7 @@ const NavBar = () => {
             </li>
           </ul>
           
+          {/* Menu button for mobile */}
           <div className='menu-button' onClick={toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M12,15H7a3,3,0,0,1-3-3V7A3,3,0,0,1,7,4h5a3,3,0,0,1,3,3v5A3,3,0,0,1,12,15Z"/>
@@ -62,4 +73,5 @@ const NavBar = () => {
   );
 };
 
+{/* Exporting the Navigation bar to make it to be able to be imported */}
 export default NavBar;
