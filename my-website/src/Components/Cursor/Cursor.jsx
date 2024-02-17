aec71203a40c4dcf0291cd08070627a0122ea20a
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-17 14:07:46 +0000

    adding comments on the cursor

--- my-website/src/Components/Cursor/Cursor.jsx
+++ my-website/src/Components/Cursor/Cursor.jsx
@@ -4,8 +4,10 @@ import './Cursor.css'
 
 const Cursor = () =>{
 
+    // State to store the current mouse position
     const [position, setPosition] = useState({x: 0,y: 0});
 
+    // Effect to update the position state on mouse move
     useEffect(() => {
         const mouseMove = (e) => {
             setPosition({ x: e.clientX, y: e.clientY})
@@ -13,11 +15,13 @@ const Cursor = () =>{
 
         window.addEventListener('mousemove', mouseMove);
 
+         // Cleanup function to remove the event listener
         return () => {
             window.removeEventListener('mousemove', mouseMove);
         };
     }, []);
 
+     // Render a motion.div with a 'cursor' class that animates based on mouse position
     return(
         <motion.div className='cursor' animate={{x:position.x-55, y:position.y-130}}></motion.div>
     );
