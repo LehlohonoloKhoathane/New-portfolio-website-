5c93d0a284ea7704b597327baa54f65b622f28cd
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-22 10:16:57 +0200

    inserting react spinners for loading

--- my-website/src/Components/Loader/Loader.jsx
+++ my-website/src/Components/Loader/Loader.jsx
@@ -1,5 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
+import { RingLoader } from 'react-spinners';
+
 import './Loader.css';
 
 const Root = () => {
@@ -9,13 +11,13 @@ const Root = () => {
       // Simulating a 2-second delay
       setTimeout(() => {
         setLoading(false);
-      }, 2000);
+      }, 4000);
     }, []);
 }  
 
 const Loader = () => {
     return (
-      <div className="loader">Loading...</div>
+      <div className="loader">  <RingLoader color="#36D7B7" loading={true} size={60} /></div>
     );
   };
   
