1193bd0d5c26592311044e29c368e8da14683d2e
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2024-02-23 13:22:08 +0200

    Another alignments on work

--- my-website/src/Components/Work/Work.jsx
+++ my-website/src/Components/Work/Work.jsx
@@ -116,17 +116,17 @@ const Work = () => {
                 <motion.div className="testimonial-items">
                     {/* Testimonial items */}
                     <motion.div className="item-testi">
-                        <h3 className="name">Aqueel Honslo, <span> Software Developer</span></h3>
+                        <h3 className="name">Aqeel Honslo, <span> Software Developer</span></h3>
                         <ImQuotesLeft className='myQuote'/>
                         <p>Lehlohonolo delivers excellence! Their coding skills & attention to detail made our project a success. Highly recommended for top-notch software development.</p><ImQuotesRight className='myQuote'/>
                     </motion.div>
                     <motion.div className="item-testi">
-                        <h3 className="name">Chuma Raxhothi,<span> Network Engineer</span></h3>
+                        <h3 className="name">Chuma Raxothi,<span> Network Engineer</span></h3>
                         <ImQuotesLeft className='myQuote'/>
                         <p>Thanks to Lehlohonolo's contributions, our project was completed on time and exceeded our expectations. I would highly recommend him to anyone looking for a talented and reliable software developer."</p><ImQuotesRight className='myQuote'/>
                     </motion.div>
                     <motion.div className="item-testi">
-                        <h3 className="name">Mookho Lebaka,<span> Client</span></h3>
+                        <h3 className="name">Mookgo Lebaka,<span> Client</span></h3>
                         <ImQuotesLeft className='myQuote'/>
                         <p>I hired Lehlohonolo to develop a custom software solution for my business, and I couldn't be happier with the results. He took the time to understand my needs and delivered a product that exceeded my expectation</p><ImQuotesRight className='myQuote'/>
                     </motion.div>
